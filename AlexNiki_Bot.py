51458e6c3caabcbefc146da03573550609f006e3
AlexNiki <alex@MacBook-Pro-Aleksander.local>
2021-02-22 15:52:31 +0300

    SQL commit

--- AlexNiki_Bot.py
+++ AlexNiki_Bot.py
@@ -1,10 +1,11 @@
 import telebot
-#import pyexcel
+import psycopg2
+
 from Config import TOKEN
-from Config import bd_array
+from Config import SQLALCHEMY_DATABASE_URI
 
 bot = telebot.TeleBot(TOKEN)
-#bd_array = pyexcel.get_array(file_name="./src/ДР.xls")
+
 
 @bot.message_handler(commands=['start', 'go'])
 def start_handler(message):
@@ -15,11 +16,15 @@ def text_handler(message):
     text = message.text
     chat_id = message.chat.id
 
-    for i, row in enumerate(bd_array):
-        for j, element in enumerate(row):
-            if element == text:
-                message = bd_array[i][0] + ' ' + bd_array[i][1] + ' ' + bd_array[i][2] + ' ' + bd_array[i][3]
-                #bd_array[i][3].strftime("%d-%m-%Y")
-                bot.send_message(chat_id, message)
+    conn = psycopg2.connect(SQLALCHEMY_DATABASE_URI)
+    cursor = conn.cursor()
+    postgreSQL_select_Query = "select * from birthdays where lastname = %s or firstname = %s or middlename = %s"
+    cursor.execute(postgreSQL_select_Query, (text, text, text))
+    bd_records = cursor.fetchall()
+
+    for row in bd_records:
+        message = row[0] + ' ' + row[1] + ' ' + row[2] + ' ' + row[3].strftime("%d-%m-%Y")
+        bot.send_message(message.chat.id,message)
+    conn.close()
 
 bot.polling()
--- Config.py
+++ Config.py
@@ -1,3 +1,5 @@
+import os
+
 TOKEN = '1661150170:AAFxO9tIQlwpSB0QgJzgkHjQjBUP_jiylEM'
 
 bd_array = [
@@ -5,3 +7,8 @@ bd_array = [
             ["Никифорова", "Алена", "Алмазовна", "02.01.1989"],
             ["Капарова", "Наташа", "Алмазовна", "29.08.1986"]
            ]
+
+if os.environ.get('DATABASE_URL') is None:
+    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost:5432/bd-alexniki-bot'
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
