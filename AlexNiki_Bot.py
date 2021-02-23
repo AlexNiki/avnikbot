bfc15ef62e0fe92b1e5904ac4293e5437c7090fc
AlexNiki <alex@MacBook-Pro-Aleksander.local>
2021-02-23 17:56:23 +0300

    v28

--- AlexNiki_Bot.py
+++ AlexNiki_Bot.py
@@ -1,11 +1,14 @@
 import telebot
 import psycopg2
+import schedule
+import datetime
+import time
+from multiprocessing.context import Process
 
 from Config import TOKEN
 from Config import SQLALCHEMY_DATABASE_URI
 
 bot = telebot.TeleBot(TOKEN)
-
 
 @bot.message_handler(commands=['start', 'go'])
 def start_handler(message):
@@ -22,9 +25,43 @@ def text_handler(message):
     cursor.execute(postgreSQL_select_Query, (text, text, text))
     bd_records = cursor.fetchall()
 
-    for row in bd_records:
-        msg = row[0] + ' ' + row[1] + ' ' + row[2] + ' ' + row[3].strftime("%d-%m-%Y")
-        bot.send_message(message.chat.id, msg)
+    if len(bd_records) == 0:
+        bot.send_message(message.chat.id, 'Ничего не найдено')
+    else:
+        for row in bd_records:
+            msg = row[0] + ' ' + row[1] + ' ' + row[2] + ' ' + row[3].strftime("%d-%m-%Y")
+            bot.send_message(message.chat.id, msg)
     conn.close()
 
+
+def check_birthday():
+    now = datetime.datetime.now()
+    conn = psycopg2.connect(SQLALCHEMY_DATABASE_URI)
+    cursor = conn.cursor()
+    postgreSQL_select_Query = "select * from birthdays where birthday = %s"
+    cursor.execute(postgreSQL_select_Query, (now.strftime("%Y-%m-%d"),))
+    bd_records = cursor.fetchall()
+
+    if len(bd_records) == 0:
+        bot.send_message(message.chat.id, 'Сегодня дней рождений нет!')
+    else:
+        for row in bd_records:
+            msg = row[0] + ' ' + row[1] + ' ' + row[2] + ' ' + row[3].strftime("%d-%m-%Y")
+            bot.send_message(message.chat.id, msg)
+
+    conn.close()
+
+schedule.every().day.at("08:00").do(check_birthday)
+
+class ScheduleMessage():
+    def try_send_schedule():
+        while True:
+            schedule.run_pending()
+            time.sleep(1)
+
+    def start_process():
+        p1 = Process(target=ScheduleMessage.try_send_schedule, args=())
+        p1.start()
+
+ScheduleMessage.start_process()
 bot.polling()
--- Config.py
+++ Config.py
@@ -2,12 +2,6 @@ import os
 
 TOKEN = '1661150170:AAFxO9tIQlwpSB0QgJzgkHjQjBUP_jiylEM'
 
-bd_array = [
-            ["Никифорова", "Ева", "Александровна", "22.06.2017"],
-            ["Никифорова", "Алена", "Алмазовна", "02.01.1989"],
-            ["Капарова", "Наташа", "Алмазовна", "29.08.1986"]
-           ]
-
 if os.environ.get('DATABASE_URL') is None:
     SQLALCHEMY_DATABASE_URI = 'postgresql://localhost:5432/bd-alexniki-bot'
 else:
